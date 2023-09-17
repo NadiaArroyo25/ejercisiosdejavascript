89b211d25bd7dddb3a877ef8f5de1db030f5cbf7
NadiaArroyo25 <niac2596@gmail.com>
2023-09-17 17:36:07 -0600

    lo demartes

---
+++ ejercicios.js
@@ -0,0 +1,29 @@
+
+
+function renderpiramide(numero) {
+    for (let i = 1; i <= numero; i++) {
+        let espacio = ' '.repeat(numero - i);
+
+
+
+
+        let asteriscos = '*'.repeat(2 * i - 1);
+        console.log(espacio + asteriscos);
+    }
+}
+
+renderpiramide(5);
+
+for (i=1; 1<=navigator; i++){
+    var line = "*".repet(i);
+      function PrinLine
+}
+
+
+
+
+
+
+
+
+
